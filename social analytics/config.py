cfdf27c8fc596effaa2938dd99b8ed23d339010f
fmung2 <fmungofa@gmail.com>
2018-07-21 12:31:35 -0700

    social analytics hw

---
+++ social analytics/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "GKbmiIxv1E30dqF28aH3cBR8F"
+consumer_secret = "O3Sfn3JqbJGPSfnBOgt0t6uLESjDGBCHluwgzqtMcRYZwo5nvo"
+access_token = "1010571134222229504-XQl3rE15HeqADbprLIxZF713NZ5T64"
+access_token_secret = "z1N6pDNzE1obYOZ3SBvnIi2zcvQafhegT2CY5bqGw70A3"
